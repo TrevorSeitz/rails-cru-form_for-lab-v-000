8386572debd388eda2e12dc0eb6c60ed69195cca
Trevor Seitz <trevinla@gmail.com>
2018-06-29 14:43:17 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,6 +17,12 @@ class SongsController < ApplicationController
         redirect_to song_path(@song)
       end
 
+      def update
+        @song = Song.find(params[:id])
+        @song.update(song_params)
+        redirect_to song_path(@genre)
+      end
+
   private
 
   def song_params(*args)
