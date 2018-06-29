0b87df79d537af68d12b36d3931becc34c9aaae7
Trevor Seitz <trevinla@gmail.com>
2018-06-29 13:22:21 +0000

    automatically backed up by learn

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,37 @@
+class GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+  end
+
+  def new
+    @genre=Genre.new
+    @genre.save
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genre = Genre.create(name: params[:name])
+    redirect_to genre_path(@genre)
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(name: params[:name])
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,25 @@
+class SongsController < ApplicationController
+
+      def index
+        @songs = Song.all
+      end
+
+      def show
+        @song = Song.find(params[:id])
+      end
+
+      def new
+        @song=Song.new
+      end
+
+      def create
+        @song = Song.create(name: params[:name], artist_id: params[:artist_id], genre_id: params[:genre_id])
+        redirect_to song_path(@song)
+      end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
+end
